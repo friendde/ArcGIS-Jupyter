8f34de941fc16eaa59e2b0c5430172e85b1d165c
friendde <friendde@gru.com>
2018-07-27 11:47:14 -0400

    added loop to list groups

--- Update Hosted Service.py
+++ Update Hosted Service.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[1]:
-
-
 import arcpy
 import os, sys
 from arcgis.gis import GIS
@@ -12,24 +7,16 @@ from getpass import getpass #to accept passwords in an interactive fashion
 from getpass import getuser
 from IPython.display import display
 
-
-# In[2]:
-
-
 # Set the default path to ArcGIS Pro projects
 prjPath = (os.environ['USERPROFILE']+'\Documents\ArcGIS\Projects\\')
 #print(prjPath)
 
-
-# In[3]:
-
-
-changePath = input(f'Default ArcGIS Project Path is: {}, change path? [Y or N]'(prjPath))
+changePath = input(f'Default ArcGIS Project Path is: {prjPath}, change path? [Y or N]')
 if changePath.upper() in "YES":
-    prjPath = input('Enter new path '')
+    prjPath = input('Enter new path ')
     os.chdir(prjPath)
 for project in os.listdir(prjPath):
-    usePrj = input(f'Use project {} [Y or N]'(project))
+    usePrj = input(f'Use project {project} [Y or N]')
     if usePrj.upper() in "YES":
         prjPath = os.path.join(prjPath,project)
         break
@@ -39,11 +26,7 @@ for root, dirs, files in os.walk(prjPath):
             aprx = os.path.join(root, file)
             display(aprx)
 
-
-# In[4]:
-
-
-response = input(f'Do you want to use the active portal - {} [Y or N]'(arcpy.GetActivePortalURL()))
+response = input(f'Do you want to use the active portal - {(arcpy.GetActivePortalURL())} [Y or N]')
 if response.upper() in "YES":
     portal = arcpy.GetActivePortalURL()
 else:
@@ -54,12 +37,14 @@ display(portal)
 user = input("enter username")
 #print(getuser())
 password = getpass()
-print(f'Connecting to {}'(portal))
 gis = GIS(portal, user, password)
+print(f'Connected to {portal}')
 
-
-# In[5]:
-
+# https://esri.github.io/arcgis-python-api/apidoc/html/arcgis.gis.toc.html#arcgis.gis.GroupManager
+# https://developers.arcgis.com/python/guide/accessing-and-managing-groups/
+# list groups and group id
+for grp in gis.groups.search():
+    print(grp.title, grp.groupid)
 
 # search and list all items owned by connected user
 query="owner:{}".format(user)
@@ -70,7 +55,7 @@ max_items=100
 search_result = gis.content.search(query,item_type,sort_field,sort_order,max_items)
 for i in range(len(search_result)):
     display(search_result[i])
-    updateResult = input(f'Update {} [Y,N]'(search_result[i].title))
+    updateResult = input(f'Update {(search_result[i].title)} [Y,N]')
     if updateResult.upper() in "YES":
         sd_fs_name = search_result[i].title
         print(sd_fs_name)
@@ -78,21 +63,14 @@ for i in range(len(search_result)):
         sdItem = gis.content.search(query,item_type="Service Definition")[0]
         print(sdItem)
         break
-
-
-# In[6]:
-
+if updateResult.upper() not in "YES":
+    print(f'All Service Definitions in your Content on {portal} were presented')
 
 # Local paths to create temporary content
-#relPath = sys.path[0]
 sddraft = os.path.join(prjPath, "{}.sddraft".format(sdItem.title))
 print(sddraft)
 sd = os.path.join(prjPath, "{}.sd".format(sdItem.title))
 print(sd)
-
-
-# In[7]:
-
 
 arcpy.env.overwriteOutput = True
 prjMap = arcpy.mp.ArcGISProject(aprx)
@@ -103,10 +81,6 @@ for prjMap in prjMaps:
     if useMap.upper(input(f'Use map {} [Y/N]'(prjMap))) in 'Y':
         m = prjMap
         break
-
-
-# In[8]:
-
 
 # Create FeatureSharingDraft and set service properties
 sharing_draft = m.getWebLayerSharingDraft("HOSTING_SERVER", "FEATURE", sdItem.title)
@@ -121,8 +95,6 @@ sharing_draft.overwriteExistingService = True
 sharing_draft.exportToSDDraft(sddraft)
 
 # Stage Service
-#sd_filename = service + ".sd"
-#sd_output_filename = os.path.join(outdir, sd)
 arcpy.StageService_server(sddraft, sd)
 
 # Share to portal
@@ -130,10 +102,6 @@ print("Uploading Service Definition...")
 arcpy.UploadServiceDefinition_server(sd, "My Hosted Services")
 
 print("Uploaded service.")
-
-
-# In[ ]:
-
 
 sdItem.share(everyone=False, org=True, groups="Energy Delivery Engineering", allow_members_to_edit=False)
 
